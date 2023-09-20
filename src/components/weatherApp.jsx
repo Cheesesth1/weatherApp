b1034399e4d98ded6fefe7b68d3313f01dd7922e
shanti basnet <shanti.basnet@mnsu.edu>
2023-09-19 21:14:37 -0700

    weather function

--- src/components/weatherApp.jsx
+++ src/components/weatherApp.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./WeatherApp.css"; // Create a CSS file for styling
 
-function Weatherapp() {
-  return <div>weatherapp</div>;
+function WeatherApp() {
+  // Step 2: Set up initial state
+  const [weatherData, setWeatherData] = useState({
+    temperature: "",
+    description: "",
+    cityName: "City Name",
+  });
+
+  return (
+    <div className="weather-app">
+      <h1>{weatherData.cityName} Weather</h1>
+      <div className="temperature">{weatherData.temperature}</div>
+      <div className="description">{weatherData.description}</div>
+      {/* Add weather condition UI elements here */}
+    </div>
+  );
 }
 
-export default Weatherapp;
+export default WeatherApp;
