cf4d6858dd6450883d97734ae5eb5da1d9657759
CHIJ KUMAR SHRESTHA <cheese_sth@yahoo.com>
2023-09-21 22:31:05 -0500

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function App() {
-  return <div>App</div>;
+function Weatherapp() {
+  return;
 }
 
-export default App;
+export default Weatherapp;
---
+++ src/components/weatherApp.jsx
@@ -0,0 +1,12 @@
+import "./weatherApp.css";
+import React, { useState } from "react";
+
+function weatherapp() {
+  return;
+  <div className={styles.main}></div>;
+  <div>
+    <div>City</div>;<div>Weather</div>;<div>Temperature</div>;
+  </div>;
+}
+
+export default weatherapp;
